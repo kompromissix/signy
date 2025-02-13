dc19bae4b75efeb085ce384950637dc872be5531
kompromiss <artemprodos@gmail.com>
2025-02-13 13:28:46 +0200

    add

---
+++ src/Main/Str3my/Str3.jsx
@@ -0,0 +1,44 @@
+import './Str3.scss'
+
+export default function Str3(){
+    return(
+        <>
+            <main className='str3'>
+                <section className='StrName'>
+                    <div>
+                        <h5>Signy / Контакты</h5>
+                    </div>
+                </section>
+                <section className='background1'>
+                    <div>
+                        <div>
+                            <h1>Наши контакты</h1>
+                            <div>
+                                <div>
+                                    <p>Мы всегда рады ответить на ваши вопросы и развивать эффективное сотрудничество по всем вопросам, связанным с электронным документооборотом.</p>
+                                    <div>
+                                        <img src="" alt="" />
+                                        <div>
+                                            <p>0 800 750 643</p>
+                                            <p>+3 044 334 56 43</p>
+                                            <p>+3 044 338 86 43</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <img src="" alt="" />
+                                        <p>infosmartsign@smarttender.biz</p>
+                                    </div>
+                                    <div>
+                                        <img src="" alt="" />
+                                        <p>пр-т Бажана, 14 А Киев, 02072</p>
+                                    </div>
+                                </div>
+                                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5cf13a3eef77e7a6c3d83e88f252ed9d19c023be96c3811e6aa55a017e5d2e41&amp;width=605&amp;height=249&amp;lang=ru_RU&amp;scroll=false"></script>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
---
+++ src/Main/Str4your/Str4.jsx
@@ -0,0 +1,11 @@
+import './Str4.scss'
+
+export default function Str4(){
+    return(
+        <>
+            <main>
+                
+            </main>
+        </>
+    )
+}
