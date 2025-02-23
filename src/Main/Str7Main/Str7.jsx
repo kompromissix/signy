4f7cc40e42a7440019b2df3fe4a0277f827794c7
Artem <artemprodos@gmail.com>
2025-02-23 22:01:25 +0200

    finish

--- src/Main/Str7Main/Str7.jsx
+++ src/Main/Str7Main/Str7.jsx
@@ -4,7 +4,11 @@ import Subscribe from '../Subscribe/Subscribe'
 import zagluska from './Str7_assets/Placeholder.png'
 import { ways2 } from '../Str5my/photo'
 import { useState } from 'react';
-
+import { ans } from './data'
+import { b4 } from './b4'
+import progr from './Str7_assets/Group1392.png'
+import { prog } from './program'
+import { ways3 } from '../Str6together/photo'
 
 function Interesting(write){
     return(
@@ -19,15 +23,113 @@ function Interesting(write){
         </>
     )
 }
-
-export default function Str7(){
+function Question(answer){
     const [isExpanded, setIsExpanded] = useState(false);
     const handleClick = () => {
         setIsExpanded(!isExpanded); // Меняем состояние на противоположное
     };
     return(
         <>
+            <div>
+                <div>
+                    <button onClick={handleClick}>
+                        {isExpanded ? '-' : '+'}
+                    </button>
+                    <p>{answer.why}</p>
+                </div>
+                {isExpanded && <p>{answer.tway}</p>}
+            </div>
+        </>
+    )
+}
+
+function Treediv(div){
+    return(
+        <>
+            <div>
+                <img src={div.img} alt="" />
+                <div>
+                    <h3>{div.h3}</h3>
+                    <p>{div.p}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+function Programm(link){
+    return(
+        <>
+            <div>
+                <h2>{link.h2}</h2>
+                <ul>
+                    <li>{link.li1}</li>
+                    <li>{link.li2}</li>
+                    <li>{link.li3}</li>
+                    <li>{link.li4}</li>
+                </ul>
+                <button type="button">{link.b}</button>
+            </div>
+        </>
+    )
+}
+
+function Podpiska(buy){
+    return(
+        <>
+            <div>
+                <div>
+                    <h3></h3>
+                    <div>
+                        <p></p>
+                        <h3><span></span></h3>
+                    </div>
+                    <button type="button"></button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default function Str7(){
+    return(
+        <>
             <main className='str7'>
+                <section className='background4'>
+                    <div>
+                        {b4.map((way) =>(
+                            <Treediv {...way}/>
+                        ))}
+                    </div>
+                </section>
+                <section className='background5'>
+                    <div>
+                        <div>
+                            <Programm {...prog[0]}/>
+                            <img src={progr} alt="" />
+                        </div>
+                        <div>
+                            <img src={progr} alt="" />
+                            <Programm {...prog[1]}/>
+                        </div>
+                    </div>
+                </section>
+                <section className='background6'>
+                    <div>
+                        <h2>Пакеты цифровых подписей </h2>
+                        <div>
+                            {ways3.map((way) =>(
+                                <Podpiska {...way}/>
+                            ))}
+                        </div>
+                    </div>
+                </section>
+                <section className='background7'>
+                    <div>
+                        <h2>5 820 125 114 </h2>
+                        <h3>подписано документов</h3>
+                        <p>Наша компания уже более 30 лет создает удобные решения для бизнеса любого размера. Мы гордимся Signy как безопасным и комфортным продуктом</p>
+                    </div>
+                </section>
                 <Swiper_block/>
                 <section className='background1'>
                     <div>
@@ -45,14 +147,13 @@ export default function Str7(){
                         <iframe width="734" height="451" src="https://rutube.ru/play/embed/7814c00c182e36cd339981096b593a03/"></iframe>
                     </div>
                 </section>
-                <section>
+                <section className='background3'>
                     <div>
                         <h2>Популярные вопросы и ответы на них</h2>
                         <div>
-                            <button onClick={handleClick}>
-                                {isExpanded ? '-' : '+'}
-                            </button>
-                            {isExpanded && <p>Текст, который появляется при нажатии на "+"</p>}
+                            {ans.map((way) =>(
+                                <Question {...way}/>
+                            ))}
                         </div>
                     </div>
                 </section>
---
+++ src/Main/Str7Main/b4.js
@@ -0,0 +1,21 @@
+import img1 from './Str7_assets/speedometer1.png'
+import img2 from './Str7_assets/Vector.png'
+import img3 from './Str7_assets/piggy-bank1.png'
+
+export const b4 = [
+    {
+        img: img1,
+        h3: "Моментально",
+        p: "Никаких задержек. 1 секунда на подписание"
+    },
+    {
+        img: img2,
+        h3: "Глобально",
+        p: "Сервис доступен 24 7 со всех устройств"
+    },
+    {
+        img: img3,
+        h3: "Экономно",
+        p: "В 30 раз дешевле чем почта и курьеры"
+    },
+]
